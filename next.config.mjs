09eba48aaf1e0454d8587c61707e1a8ad8ca1fbc
Paloma Araujo <paloma.lopes@escale.com.br>
2024-08-30 23:57:31 -0300

    chore: add base next boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
+  images: {
+    domains: ['i.annihil.us'],
+  },
+});
 
 export default nextConfig;
